5e4a585b172be990680489e28a47d6ef5f4df49d
bkopfdev <bkopf8@gmail.com>
2026-03-25 17:03:13 +0900

    Finally fixed game over

--- Assets/Ben Testing/GoToMainMenu.cs
+++ Assets/Ben Testing/GoToMainMenu.cs
@@ -1,17 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Unity.Netcode;
+using System.Collections;
 
 public class GoToMainMenu : NetworkBehaviour
 {
     public void GoToMainButton()
     {
-        // NetworkManager.Singleton.Shutdown();
-        // SceneManager.LoadScene("MainMenu");
-        NetworkManager.Singleton.SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
-        // NetworkManager.Singleton.Shutdown();
+        if (!IsServer) return;
 
-        // Application.Quit();
-        //  System.Diagnostics.Process.Start(Application.dataPath.Replace("_Data", ".exe")); //new program Application.Quit(); //kill current process
+        NetworkManager.Singleton.SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+        StartCoroutine(ShutdownAfterSceneLoad());
+    }
+
+    private IEnumerator ShutdownAfterSceneLoad()
+    {
+        // Wait one frame (or more if needed)
+        yield return null;
+        NetworkManager.Singleton.Shutdown();
     }
 }
