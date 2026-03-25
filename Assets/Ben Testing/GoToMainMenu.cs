f5ff02d8842b6ebcc22e63a3bd8f002b9c8ec2ec
bkopfdev <bkopf8@gmail.com>
2026-03-25 16:54:51 +0900

    Synced Game Over

--- Assets/Ben Testing/GoToMainMenu.cs
+++ Assets/Ben Testing/GoToMainMenu.cs
@@ -6,8 +6,11 @@ public class GoToMainMenu : NetworkBehaviour
 {
     public void GoToMainButton()
     {
+        // NetworkManager.Singleton.Shutdown();
+        // SceneManager.LoadScene("MainMenu");
+        NetworkManager.Singleton.SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
         NetworkManager.Singleton.Shutdown();
-        SceneManager.LoadScene("MainMenu");
+
         // Application.Quit();
         //  System.Diagnostics.Process.Start(Application.dataPath.Replace("_Data", ".exe")); //new program Application.Quit(); //kill current process
     }
